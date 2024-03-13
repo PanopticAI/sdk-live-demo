a9eb0883b2b9af300b3916479b66de43fff4c85f
Eddie Lau <eddie@touchutility.com>
2024-03-14 07:40:13 +0800

    Update.

---
+++ docs/2.4.0/js/200.bc6c1fb3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_vital_sign_app"]=self["webpackChunkvue_vital_sign_app"]||[]).push([[200],{8200:function(e,t,a){a.d(t,{DummyFaceDetector:function(){return s}});var n=a(7580);class s{async loadModels(){}async detect(e){let t=(0,n.t)(e),a=.7,s=.5;return{faceBox:{xCenter:.5,yCenter:.5,width:t.height*s*a/t.width,height:s},landmarks:[],videoFrame:t}}}}}]);
+//# sourceMappingURL=200.bc6c1fb3.js.map
