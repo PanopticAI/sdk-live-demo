4e048c98d1f902176ff573d1f9724e2abc542af2
Eddie Lau <eddie@touchutility.com>
2024-03-14 10:10:26 +0800

    added version 2.4.0

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rm -rf docs/1.0.9
 cp -a ../test/vue-vital-sign-app-1.0.9/dist ./docs/1.0.9
+rm -rf docs/2.4.0
+cp -a ../test/vue-vital-sign-app-2.4.0/dist ./docs/2.4.0
 git add docs
 git commit -m "Update."
 git push
