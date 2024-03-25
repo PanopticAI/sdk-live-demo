16b8eaae5463310a11ae3729367827afe19b17a3
Eddie Lau <eddie@touchutility.com>
2024-03-25 10:35:43 +0800

    added 2.4.1

---
+++ docs/2.4.1/js/791.546525fb.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_vital_sign_app"]=self["webpackChunkvue_vital_sign_app"]||[]).push([[791],{6791:function(e,t,a){a.d(t,{DummyFaceDetector:function(){return s}});var n=a(5254);class s{async loadModels(){}async detect(e){let t=(0,n.t)(e),a=.7,s=.5;return{faceBox:{xCenter:.5,yCenter:.5,width:t.height*s*a/t.width,height:s},landmarks:[],videoFrame:t}}}}}]);
+//# sourceMappingURL=791.546525fb.js.map
