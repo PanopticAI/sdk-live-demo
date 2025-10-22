03bca42bcd217bcd3cf3003418e21f9a709f4595
Eddie Lau <eddie@touchutility.com>
2025-10-22 17:00:01 +0800

    fixed the base path problem for vue demo

--- docs/vue/3.0.0-b12/assets/CeRGY6Jb-DJyPDQ45.js
+++
@@ -1,1 +0,0 @@
-import{X as e}from"./index-ySnX5Fxk.js";const t={},o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),l=e(o);export{l as a};
---
+++ docs/vue/3.0.0-b13/assets/DXja69_O-CpyH5ASY.js
@@ -0,0 +1,1 @@
+import{X as e}from"./index-B5cHlXDj.js";const t={},o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),l=e(o);export{l as a};
