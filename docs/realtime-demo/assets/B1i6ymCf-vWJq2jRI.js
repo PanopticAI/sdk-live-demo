1841cc0916df507673c49e50426dd87d88e0fa4c
Eddie Lau <eddie@touchutility.com>
2025-11-25 17:20:00 +0800

    Deploy MyVitalsPlus realtime demo
    - Update to latest version
    - Base path: /sdk-live-demo/realtime-demo/
    - Built from ts-vital-sign-camera example14

---
+++ docs/realtime-demo/assets/B1i6ymCf-vWJq2jRI.js
@@ -0,0 +1,1 @@
+import{S as e}from"./index-bB6-Pi5A.js";const t={},o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),l=e(o);export{l as a};
--- docs/realtime-demo/assets/CHa2WaO_-D1LqSrQk.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-BDEnCT1Q.js";const t={},o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),l=e(o);export{l as a};
